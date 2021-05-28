6ef26f3766eef9ba870c24c3f14afba6f1c6426c
ruppo-912116 <newruppo123@gmail.com>
2021-05-28 17:13:21 +0545

    getting lead list from the database integrated

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const landing = require("../controllers/landing");
 /* GET home page. */
 router.get('/',landing.get_landing);
 router.post('/', landing.submit_lead);
+router.get('/leads', landing.show_leads);
 
 module.exports = router;
