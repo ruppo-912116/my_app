0a019897bba9b911a269dc45b2ade985a3130330
ruppo-912116 <newruppo123@gmail.com>
2021-05-28 14:10:56 +0545

    added landing page

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+const landing = require("../controllers/landing");
+
+/* GET home page. */
+router.get('/',landing.get_landing);
+
+module.exports = router;
